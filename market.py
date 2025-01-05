5941c4735049edbf0df916720a112150bcad0763
Kayce Basques <kayce@google.com>
2025-01-04 22:41:23 -0800

    Add career and market data

--- market.py
+++ market.py
@@ -1,6 +1,6 @@
 import json
 
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
 
 entry = {}
 
@@ -11,26 +11,26 @@ if not url.startswith(expected_prefix):
 tokens = url.split('/')
 entry['post_id'] = tokens[6]
     
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(url, wait_until='domcontentloaded')
-    # title
-    title_node = page.query_selector('h1')
-    entry['title'] = title_node.text_content().strip()
-    # date
-    entry['month'] = input('Month: ')
-    entry['year'] = input('Year: ')
-    # location
-    continent = input('Continent: ')
-    entry['continent'] = None if continent == '' else continent
-    country = input('Country: ')
-    entry['country'] = None if country == '' else country
-    country = input('State: ')
-    entry['state'] = None if state == '' else state
-    city = input('City: ')
-    entry['city'] = None if city == '' else city
-    browser.close()
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False)
+#     page = browser.new_page()
+#     page.goto(url, wait_until='domcontentloaded')
+#     # title
+#     title_node = page.query_selector('h1')
+#     entry['title'] = title_node.text_content().strip()
+#     # date
+#     entry['month'] = input('Month: ')
+#     entry['year'] = input('Year: ')
+#     # location
+#     continent = input('Continent: ')
+#     entry['continent'] = None if continent == '' else continent
+#     country = input('Country: ')
+#     entry['country'] = None if country == '' else country
+#     country = input('State: ')
+#     entry['state'] = None if state == '' else state
+#     city = input('City: ')
+#     entry['city'] = None if city == '' else city
+#     browser.close()
 
 # save the data
 with open('market.json', 'r') as f:
